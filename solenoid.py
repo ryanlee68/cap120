197a326b14baeabd6437f967c034adb79cbc200d
Ryan Lee <ryanlee619@gmail.com>
2025-04-08 13:29:37 -0700

    uhih

--- solenoid.py
+++ solenoid.py
@@ -4,22 +4,40 @@ import requests
 
 # Connect to the robot
 robot = Dorna()
-robot.connect("169.254.81.54")  # Replace with your robot's IP address
 
-# Function to set digital output
-def set_output(pin, state):
-    command = {
-        'cmd': 'output',
-        'out{}'.format(pin): state
-    }
-    robot.play(command)
+robot.connect("169.254.81.54")
 
-# Activate the solenoid
-set_output(0, 0)  # Set output 0 to logic low to turn on the solenoid
-time.sleep(2)     # Keep the solenoid on for 2 seconds
-
-# Deactivate the solenoid
-set_output(0, 1)  # Set output 0 to logic high to turn off the solenoid
-
-# Disconnect from the robot
+# unsuck
+# robot.output(0,1)
+# suck
+robot.output(0,0)
 robot.close()
+
+
+
+# 🟢[21:16:18]{"id":236210,"stat":2}
+# 🟢[21:16:18]{"id":252382,"stat":0}
+# 🟢[21:16:18]{"id":856144,"stat":-1}
+# 🟢[21:16:18]{"id":722584,"stat":0}
+# 🟢[21:16:18]{"id":722584,"stat":1}
+# 🟢[21:16:18]{"cmd":"axis","id":722584,"ratio5":1,"ratio6":20,"ratio7":20}
+# 🟢[21:16:18]{"id":722584,"stat":2}
+# 🟢[21:16:18]{"id":252382,"stat":1}
+# 🟢[21:16:18]{"cmd":"uid","id":252382,"uid":"310024001751313232343031"}
+# 🟢[21:16:18]{"id":252382,"stat":2}
+# 🟢[21:16:19]{"cmd":"alarm","alarm":1,"err0":0,"err1":0,"err2":0,"err3":0,"err4":0,"err5":-744,"err6":0,"err7":0}
+
+# 🟢[21:17:11]{"id":311345,"stat":2}
+# 🟢[21:17:11]{"id":215797,"stat":-1}
+# 🟢[21:17:11]{"id":705315,"stat":0}
+# 🟢[21:17:11]{"id":705315,"stat":1}
+# 🟢[21:17:11]{"cmd":"axis","id":705315,"ratio5":1,"ratio6":20,"ratio7":20}
+# 🟢[21:17:11]{"id":705315,"stat":2}
+# 🟢[21:17:11]{"id":442236,"stat":1}
+# 🟢[21:17:11]{"cmd":"adc","id":442236,"adc0":29460,"adc1":42956,"adc2":37766,"adc3":37814,"adc4":37799}
+# 🟢[21:17:11]{"id":442236,"stat":2}
+# 🟢[21:17:11]{"id":60018,"stat":1}
+# 🟢[21:17:11]{"cmd":"uid","id":60018,"uid":"310024001751313232343031"}
+# 🟢[21:17:11]{"id":60018,"stat":2}
+
+# {"cmd":"pwm","id":863583,"pwm0":0,"pwm1":0,"pwm2":0,"pwm3":0,"pwm4":0,"pwm5":0"duty0":0,"duty1":0,"duty2":0,"duty3":0,"duty4":0,"duty5":0,"freq0":0,"freq1":0,"freq2":0,"freq3":0,"freq4":0,"freq5":0}
