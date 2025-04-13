f49a51d12aae1bb25a2084cb840f0f65e717659e
akuang591 <alankuang591@gmail.com>
2025-04-12 21:49:20 -0700

    added e stop, debug aux axis

---
+++ estoptest.py
@@ -0,0 +1,12 @@
+from dorna2 import Dorna
+import time
+# Connect to the robot
+robot = Dorna()
+robot.connect("169.254.154.191")
+
+while True:
+    if robot.get_input(7) == 0:
+        print("e stop activated")
+    elif robot.get_input(7) == 1:
+        print("e stop not activated")
+    time.sleep(1) 
